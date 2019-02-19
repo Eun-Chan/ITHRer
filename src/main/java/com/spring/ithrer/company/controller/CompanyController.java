320b6e54315f0e95a1cb27a402cf689f4c89215b
pillidog815 <pillidog815@gmail.com>
2019-02-19 22:04:19 +0900

    채용공고 등록페이지 연결

--- src/main/java/com/spring/ithrer/company/controller/CompanyController.java
+++ src/main/java/com/spring/ithrer/company/controller/CompanyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -105,12 +106,9 @@ public class CompanyController {
 	}
 	
 	@GetMapping("/recruitmentAdd.ithrer")
-	public ModelAndView recruitmentAddView(ModelAndView mav)
-	{
-		logger.debug("공고등록 페이지 요청");
+	public ModelAndView recruitmentAddView(ModelAndView mav) {
 		mav.setViewName("company/recruitmentAdd");
 		return mav;
 	}
-	
 	
 }
