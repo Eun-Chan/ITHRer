4eeb28499c0f7100dc1fce3a0759e7c06e3b41cc
MyoungHoon Jung <mh.jung.dev@gmail.com>
2019-03-06 17:47:03 +0900

    etc

--- src/main/java/com/spring/ithrer/board/controller/Cron.java
+++ src/main/java/com/spring/ithrer/board/controller/Cron.java
@@ -13,10 +13,10 @@ public class Cron {
 	/*
      * 5초 마다 실행
      */
-    @Scheduled(fixedDelay=5000)
-    public void init() {
-        System.out.println("init");
-    }
+//    @Scheduled(fixedDelay=5000)
+//    public void init() {
+//        System.out.println("init");
+//    }
 
     /**
      * 매일 22시에 수행
