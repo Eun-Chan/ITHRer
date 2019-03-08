dffcec59e4d94942bc5bdf489952a0b2c070e480
MyoungHoon Jung <mh.jung.dev@gmail.com>
2019-03-08 18:06:27 +0900

    지원자뷰단에서 사용할 필드 추가

--- src/main/java/com/spring/ithrer/resume/model/vo/Profile.java
+++ src/main/java/com/spring/ithrer/resume/model/vo/Profile.java
@@ -24,4 +24,9 @@ public class Profile implements Serializable{
 	private String email;
 	private String phone;
 	private String address;
+	private String memberId;
+	// 나이 저장용 필드
+	private int age;
+	// 기업이 회원 즐겨찾기용 필드(즐찾했으면 1 아니면 0)
+	private int count;
 }
