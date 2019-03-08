17af6b4528a1191b874641ae01f36187035af271
jang <jsw2562@gmail.com>
2019-03-08 20:31:51 +0900

    error 패이지 추가 / 인터페이스 추가 (회사로그인 안하고 디테일 볼때)  (선웅)

--- src/main/java/com/spring/ithrer/resume/model/vo/Profile.java
+++ src/main/java/com/spring/ithrer/resume/model/vo/Profile.java
@@ -17,11 +17,16 @@ public class Profile implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String userResumeTitle;
+	private String userresumetitle;
 	private String name;
 	private String birth;
 	private String gender;
 	private String email;
 	private String phone;
 	private String address;
+	private String memberId;
+	// 나이 저장용 필드
+	private int age;
+	// 기업이 회원 즐겨찾기용 필드(즐찾했으면 1 아니면 0)
+	private int count;
 }
